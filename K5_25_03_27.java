e14dc0e7cdf2272007c51434b0158b203acbf04c
HoneyMilkilly <the1213k@gmail.com>
2025-03-27 16:32:43 +0900

    업 캐스팅 주석 부분 좀 수정

--- K5_25_03_27.java
+++ K5_25_03_27.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class K5_25_03_27{ //업 캐스팅 관련 코드 부모클래스와 자식클래스의 이름이 같은 메서드 중 
     public static void main(String[] args)
     {
-        Animall a = new Dog();//new Dog는 Animall a의 객체이고, a는 클래스 Animall의 참조형 변수가 됨
-        a.sound();
+        Animall a = new Dog();//new Dog는 Animall a의 객체이고, a는 클래스 Animall의 참조형 변수가 됨, 참조형 변수 a는 new Dog를 참조
+        a.sound();//참조형 변수 a의 주소값의 메서드 sound를 실행한다.
         // a.wagTail(); -> 이 코드는 실행되지 앟는다 
     }
 }
