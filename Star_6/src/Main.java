ace39f360b464a65db444fcff7bcf38fbd7a77eb
HoneyMilkilly <the1213k@gmail.com>
2025-03-31 15:44:52 +0900

    테스트

--- Star_6/src/Main.java
+++ Star_6/src/Main.java
@@ -1,11 +1,13 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.Scanner;//백준 1271번
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        System.out.println(a+b);
+        BigInteger a = scan.nextBigInteger();
+        BigInteger b = scan.nextBigInteger();
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
     }
     }
