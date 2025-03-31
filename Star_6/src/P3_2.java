93ce781a6e9106072359b1767b99f4ef15637f6f
HoneyMilkilly <the1213k@gmail.com>
2025-03-31 18:13:41 +0900

    남궁성 선생님 3-2번 문제

---
+++ Star_6/src/P3_2.java
@@ -0,0 +1,8 @@
+public class P3_2 {
+    public static void main(String[] args) {
+        int num = 123; //사과의 총량
+        int num2 = 10; //나눠 가져야 할 사람
+        int result = num/num2+1; //3개의 사과를 버릴 순 없기에 +1을 해준다.
+        System.out.println(result + "개의 바구니가 필요하다");
+    }
+}
