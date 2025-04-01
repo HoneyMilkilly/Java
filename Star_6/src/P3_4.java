858b5fcee181ff92dde2fb4c8fe6a99873a97082
HoneyMilkilly <the1213k@gmail.com>
2025-04-01 15:32:52 +0900

    남궁성 선생님 3-4번 문제

---
+++ Star_6/src/P3_4.java
@@ -0,0 +1,6 @@
+public class P3_4 {
+    public static void main(String[] args) {//남궁성 선생님 문제 3-4
+        int num = 456;
+        System.out.println((num/100)*100);// 정수형이고 나누기 100을 하면 나머지값은 날아가고, 몫만 나옴
+    }
+}
