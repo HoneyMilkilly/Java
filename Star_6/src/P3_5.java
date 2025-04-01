3290f20db37a1f710a391ec49bf8673d07f2c1fb
HoneyMilkilly <the1213k@gmail.com>
2025-04-01 15:46:40 +0900

    남궁성 선생님 3-5번 문제

---
+++ Star_6/src/P3_5.java
@@ -0,0 +1,9 @@
+public class P3_5 {
+    public static void main(String[] args) {//남궁성 선생님 3-5번 문제
+        int num = 333;
+        int result = num / 10 * 10;
+        System.out.println(result);
+        result += 1;
+        System.out.println(result);
+    }
+}
