70f143aef93606bb88a795778b9b91b9f1605085
HoneyMilkilly <the1213k@gmail.com>
2025-04-07 18:07:52 +0900

    String 클래스 및 자주쓰는 메서드 5개

--- Star_6/src/P25_04_07_02.java
+++ Star_6/src/P25_04_07_02.java
@@ -1,13 +1,22 @@
 public class P25_04_07_02 {
     public static void main(String[] args) {
-        Loop1 : for (int i = 1; i <= 9; i++){
-           Loop2: for (int j = 1; j <= 10; j++){
-                    System.out.println(i + "*" + j + "=" + i*j);
-                    if (i == 4 && j == 10){//구구단을 4단까지만 출력하고 빠져나옴!
-                        break Loop1;
-                    }
-            }
-            System.out.println(i);
+        String str = "ABCDE";
+        char ch = str.charAt(4);//String 배열의 4번째 칸 E를 문자열 변수 ch에 저장!
+        String str2 = str.substring(1,4);//
+        String str3 = str.substring(1);
+        String str4 = str.substring(1, str.length());
+        String str5 = "Apple";
+        String str6 = "APPle";
+        char[] arr = str5.toCharArray();
+
+        System.out.println(str);//str 문자열 변수(객체)를 출력함
+        System.out.println(ch); //ch 문자형 변수를 출력함
+        System.out.println(str2); //인덱스 1번 ~ 3번까지 출력함
+        System.out.println(str3); //인덱스 1번부터 끝까지 쫙 출력함
+        System.out.println(str4); //인덱스 1번부터 str.length()함수를 사용해 str 인덱스 크기까지 출력함
+        System.out.println(str5.equals(str6));
+        System.out.println(arr);
+        System.out.println(arr[1]);
+
         }
     }
-}
